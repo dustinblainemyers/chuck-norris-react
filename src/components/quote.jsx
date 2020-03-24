3cc0eaaab132c26eeef20909ec8b62299077bf61
Dustin Myers <dustinblaine.myers@gmail.com>
2020-03-24 15:52:18 -0400

    having a bit of trouble with this.setState

--- src/components/quote.jsx
+++ src/components/quote.jsx
@@ -2,9 +2,10 @@ import React, { Component } from "react";
 
 class Quote extends Component {
     componentDidMount() {
+        console.log("quote component has mounted");
         this.setState(
 
-          { quote =  "This is one quote"}
+          { quote :  "This is one quote"}
         )
     }
   render() {
