d3410381dd249e8c02e05a52444addfdffdfa9a8
Dustin Myers <dustinblaine.myers@gmail.com>
2020-03-24 14:33:22 -0400

    more practice with modal

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -2,6 +2,16 @@ import React, { Component } from "react";
 
 class Modal extends Component {
   render() {
+    
+    const closeModalButton = document.querySelector('#closeModal');
+
+    closeModalButton.addEventListener('click', function(e) {
+
+        const modalWindow = document.querySelector('.modal-overlay');
+        modalWindow.classList.toggle('open');
+    })
+
+
     return (
       <div className="modal-overlay">
         <div className="modal-content">
