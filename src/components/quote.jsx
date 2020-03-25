b8c32cc4ecd546e83f98911c50c39e14abd9413b
Dustin Myers <dustinblaine.myers@gmail.com>
2020-03-25 13:27:49 -0400

    console logging data from chuck api works

--- src/components/quote.jsx
+++ src/components/quote.jsx
@@ -4,14 +4,17 @@ class Quote extends Component {
   state = {
     quote: 'Fetching quote...'
   };
-    componentDidMount() {
-       
-        setTimeout(() => {
-          this.setState(
+    async componentDidMount() {
+       try {
+        const response = await fetch('https://api.chucknorris.io/jokes/random?category=dev');
+        const data = await response.json();
+        console.log("data is ", data);
 
-            {quote = "yep"};
-          )
-        }, 5000);
+
+      } catch (error) {
+          return error;
+
+      }
         
     }
   render() {
